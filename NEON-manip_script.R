38a35c6cb8ccc0219fe7d3d2ae9f2e577fcf8644
vinceng1802 <vinceng1802@hotmail.com>
2019-04-23 13:07:26 -0400

    Added USGS data and updated figure

--- NEON-manip_script.R
+++ NEON-manip_script.R
@@ -9,28 +9,27 @@
 #########
 
 ##
-#### SET WORKING DIRECTORY 
-##
-
-dir <- "C:/Users/Vince/Desktop/Zipkin Lab/NEON/NEON-download"
-setwd(dir)
-
-##
 #### INSTALL PACKAGES
 ##
 
 #install.packages("lubridate")
 #install.packages("writexl")
 #install.packages("tidyverse")
+#install.packages("ggpubr")
 
 library(lubridate)
 library(writexl)
 library(tidyverse)
 library(ggplot2)
+library(ggpubr)
+library(dplyr)
 
 ##
-#### IMPORTING DATA
+#### SET WORKING DIRECTORY AND IMPORT NEON DATA
 ##
+
+dir <- "C:/Users/Vince/Desktop/Zipkin Lab/NEON/NEON-download"
+setwd(dir)
 
 # Credit to ANG from Stackoverflow for code to import data
 # https://stackoverflow.com/questions/46682818/read-a-csv-file-in-a-zipped-folder-with-r-without-unzipping
@@ -39,26 +38,40 @@ library(ggplot2)
 birds <- as.character(unzip("Birds.zip", list = TRUE)$Name)
 # LOAD COUNT DATA
 birCount <- read.csv(unz("Birds.zip", "Birds/brd_countdata.csv"), header = TRUE,
-                 sep = ",") 
+                     sep = ",") 
 # LOAD PER PLOT DATA
 birPlot <- read.csv(unz("Birds.zip", "Birds/brd_perpoint.csv"), header = TRUE,
-                 sep = ",") 
+                    sep = ",") 
 
 # MAMMAL DATA 
 mammals <- as.character(unzip("Mammals2018.zip", list = TRUE)$Name)
 # LOAD PLOT DATA
 mamPlot <- read.csv(unz("Mammals2018.zip", "Mammals2018/mam_perplotnight.csv"), header = TRUE,
-                          sep = ",") 
+                    sep = ",") 
 # LOAD TRAP DATA
 mamTrap <- read.csv(unz("Mammals2018.zip", "Mammals2018/mam_pertrapnight.csv"), header = TRUE,
-                           sep = ",")
+                    sep = ",")
 
 # LOAD FIELD SITES
 fieldSites <- read.csv(unz("field-sites.zip", "field-sites.csv"), header = TRUE,
                        sep = ",")
 
+##
+#### SET WORKING DIRECTORY AND IMPORT HUBBARD BROOK DATA
+##
+
+setwd("C:/Users/Vince/Desktop/Zipkin Lab/NEON/FW__foliage_gleaning_bird_dataset")
+birdCount <- readRDS("Abundance_array_Zipkin_species.rds")
+
+##
+#### SET WORKING DIRECTORY AND IMPORT BBS DATA
+##
+
+setwd("C:/Users/Vince/Desktop/Zipkin Lab/NEON/USGS_BBS-dataset")
+load("countBBS.R")
+
 #########
-## PART - DATA EXPLORATION
+## PART - DATA EXPLORATION of NEON
 #########
 
 # CALCULATE PLOTS PER SAMPLING SITE FOR MAMMALS
@@ -105,28 +118,28 @@ nrow(mergedALL[mergedALL$Bir == 0 & mergedALL$Mam == 1,])
 ##
 
 #Subsetting columns of interest for Vince
-bartBird <- birCount[birCount$siteID == 'BART', c('siteID','plotID','pointID','startDate','pointCountMinute','vernacularName','clusterSize')]
-target <- c('Black-throated Green Warbler',
-            'Black-throated Blue Warbler',
-            'Ovenbird',
-            'Red-eyed Vireo',
-            'Blackburnian Warbler',
-            'Myrtle Warbler',
-            'Blue-headed Vireo',
-            'Magnolia Warbler',
-            'American Redstart',
-            'Blackpoll Warbler',
-            'Canada Warbler',
-            'Black-and-white Warbler',
-            'Nashville Warbler')
-bartBird <- bartBird %>% filter(vernacularName %in% target)
+bartBird <- birCount[birCount$siteID == 'BART', c('siteID','plotID','pointID','startDate','pointCountMinute','taxonID','vernacularName','clusterSize')]
+target <- c('AMRE',
+            'BAWW',
+            'BHVI',
+            'BLBW',
+            'BLPW',
+            'BTBW',
+            'BTNW',
+            'CAWA',
+            'MAWA',
+            'MYWA',
+            'NAWA',
+            'OVEN',
+            'REVI')
+bartBird <- bartBird %>% filter(taxonID %in% target)
 
 # levels(bartBird$vernacularName) <- c(levels(bartBird$vernacularName), 
 #                                      'Myrtle Warbler', 'Nashville Warbler', 'Blackpoll Warbler')
 # bartBird$vernacularName
 
 ##
-#### MANAGE DATA 
+#### MANAGE BART DATA 
 ##
 
 #bartBird
@@ -142,18 +155,18 @@ bartBird$month <- month(bartBird$startDate)
 bartBird$day <- day(bartBird$startDate)
 
 ## Add dummy rows of missing species
-bartBird <- bartBird %>% select(siteID, plotID, pointID, vernacularName, clusterSize, year, month, day) 
+bartBird <- bartBird %>% select(siteID, plotID, pointID, taxonID, clusterSize, year, month, day) 
 missSpp <- data.frame(siteID = rep('BART',3), plotID=rep('BART_025'), pointID=rep("C1",3), 
-                      vernacularName=c('Myrtle Warbler', 'Nashville Warbler', 'Blackpoll Warbler'), 
+                      taxonID=c('MYWA', 'NAWA', 'BLPW'), 
                       clusterSize = rep(0, 3), year=rep(2015, 3), month=rep(6, 3), day=rep(14,3))
 
 bartBird <- rbind(bartBird,missSpp)
 
 
 #Reset Factor levels
-bartBird$vernacularName <- droplevels(bartBird$vernacularName)
+bartBird$taxonID <- droplevels(bartBird$taxonID)
 #Determining birds found per species
-bartBirdSum <- aggregate(bartBird$clusterSize, by = list(bartBird$vernacularName), FUN = function(x) sum(x))
+bartBirdSum <- aggregate(bartBird$clusterSize, by = list(bartBird$taxonID), FUN = function(x) sum(x))
 colnames(bartBirdSum) <- c("Species", "Count")
 
 #Change NAs to 0
@@ -163,11 +176,102 @@ bartBirdSum[is.na(bartBirdSum)] <- 0
 #write_xlsx(x = bartBird, path = "bartBird.xlsx")
 
 #Select species of interest
-ggplot(data= bartBirdSum, aes(x=Species, y=Count, fill=Species)) + 
-  geom_bar(stat="identity") +
-  theme(axis.text.x=element_blank(),
-          axis.ticks.x=element_blank())
+aa <- ggplot(data= bartBirdSum, aes(x=Species, y=Count, fill=Species)) + 
+        geom_bar(stat="identity") +
+        ggtitle("NEON (2015-2018)") +
+        scale_y_continuous(breaks=seq(0,400,100)) +
+        theme(plot.title=element_text(size = 8), 
+              axis.text.x=element_blank(),
+              axis.text.y=element_text(size = 6),
+              axis.ticks.x=element_blank(),
+              axis.title.x=element_blank(),
+              axis.title.y=element_blank(),
+              legend.text=element_text(size=6),
+              legend.title=element_blank(), 
+              legend.position="bottom"
+              ) +
+        guides(fill = guide_legend(nrow = 2))
+       
 
+##
+#### Hubbard Book Data Manipulate and Plotting
+##
+
+#Finding Taxon IDs
+birdCount[1,1,1,]
+
+birdCount <- birdCount[,,12:20,]
+count <- as.data.frame(apply(birdCount, c(4), sum, na.rm=T))
+colnames(count) <- "Count"
+hubCount <- data.frame(Count = count$Count, Spp = rownames(count), DataSource = rep("Hubbard Brook",13))
+
+bb <- ggplot(data= hubCount, aes(x=Spp, y=Count, fill=Spp)) + 
+        geom_bar(stat="identity") +
+        ggtitle("LTER (2010-2018)") +
+        labs(x="Species", fill = "Species") +
+        scale_y_continuous(breaks=seq(0,5000,2000)) +
+        theme(plot.title=element_text(size = 8),
+              axis.text.x=element_blank(),
+              axis.text.y=element_text(size = 6),
+              axis.ticks.x=element_blank(),
+              axis.title.x=element_blank(),
+              axis.title.y=element_blank()
+              )
+
+##
+#### USGS Data Manipulation and Plotting
+##
+
+cc <- ggplot(data=countBBS, aes(x=taxonID, y=Count, fill=taxonID)) + 
+  geom_bar(stat="identity") +
+  ggtitle("USGS (2010-2017)") +
+  scale_y_continuous(breaks=seq(0,7000,2000)) +
+  theme(plot.title=element_text(size = 8),
+        axis.text.x=element_blank(),
+        axis.text.y=element_text(size = 6),
+        axis.ticks.x=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.position = "none")
+
+##
+#### Ebird Data Manipulation and Plotting
+##
+
+dd <- ggplot(data= bartBirdSum, aes(x=Species, y=Count, fill=Species)) + 
+  geom_bar(stat="identity") +
+  ggtitle("eBird (2010-2018)") +
+  scale_y_continuous(breaks=seq(0,400,100)) +
+  theme(plot.title=element_text(size = 8),
+        axis.text.x=element_blank(),
+        axis.text.y=element_text(size = 6),
+        axis.ticks.x=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.position = "none")
+
+
+##
+#### Joining Plots from different data sources together
+##
+
+
+## ALL TOGETHER TO MAKE PUBLICATION QUALITY FIGURE
+#Set Working Directory
+setwd("C:/Users/Vince/Desktop/Zipkin Lab/NEON/NEON-download")
+#Set image specifications
+png('Figure1.png', width = 3.9, height = 2.86, units = 'in', res = 450)
+#Make plot
+figure <- ggarrange(aa, bb, cc, dd,
+                    ncol = 2, nrow = 2, common.legend = T, legend = "bottom")
+#ggarrange(bbaa,covs_rich,nrow=2,heights=c(1,1)) #meta_all <- 
+
+#figure <- annotate_figure(figure, left=text_grob('Count', rot = 90), 
+#                          bottom=text_grob('Species'))
+figure
+#Save plot
+dev.off()
+#1000*700
 
 ##
 #### SRER MAMMAL DATA EXPLORATION
